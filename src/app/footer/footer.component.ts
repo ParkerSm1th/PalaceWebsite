eb82cce19ad916ffd596d71fe485802dbf9879fb
Vidsify <Vidsify@hotmail.com>
2019-03-08 17:54:24 +0000

    Footer
    Final design for Footer.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faYoutubeSquare } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'pl-footer',
@@ -6,6 +10,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  faTwitterSquare = faTwitterSquare;
+  faFacebookSquare = faFacebookSquare;
+  faInstagram = faInstagram;
+  faYoutubeSquare = faYoutubeSquare;
 
   constructor() { }
 
