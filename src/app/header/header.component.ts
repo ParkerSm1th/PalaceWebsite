dea2acde5125d3705d876f002a705deed1fa2bd1
Isaac Goodwin(Flakenstine) <isaacgoodwin97@gmail.com>
2019-03-18 18:09:43 -0600

    Updated WIP message at top of website

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'pl-header',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   routes;
+  faInfoCircle = faInfoCircle;
 
   constructor() { }
 
