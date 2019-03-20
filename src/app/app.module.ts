794beea4f92f3f9e7277d8c01b32dc46d4c2e7e2
Vidsify <vidsify@hotmail.com>
2019-03-20 18:54:03 +0000

    Remove MinecraftComponent
    Will rewrite at a later date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { MinecraftComponent } from './minecraft/minecraft.component';
 import { PlayersComponent } from './players/players.component';
 import { PlayersSearchComponent } from './players-search/players-search.component';
 
@@ -17,7 +16,6 @@ import { PlayersSearchComponent } from './players-search/players-search.componen
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    MinecraftComponent,
     PlayersComponent,
     PlayersSearchComponent
   ],
