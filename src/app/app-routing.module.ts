d7cc1af22b9702eb48a559850d6d09e76bc28263
Isaac Goodwin(Flakenstine) <isaacgoodwin97@gmail.com>
2019-04-13 15:15:54 -0600

    Fixed issues with the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PlayersComponent } from './players/players.component';
 import { StafflistComponent } from './stafflist/stafflist.component';
+import { PlayComponent } from './play/play.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'players', component: PlayersComponent },
+  { path: 'play', component: PlayComponent },
   { path: 'staff', component: StafflistComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
