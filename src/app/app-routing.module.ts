3d8d820e8432b13fb9353c8443069436a26da26a
Isaac Goodwin(Flakenstine) <isaacgoodwin97@gmail.com>
2019-04-13 15:43:31 -0600

    Fixed router again... lol

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { PlayersComponent } from './players/players.component';
 import { StafflistComponent } from './stafflist/stafflist.component';
 import { PlayComponent } from './play/play.component';
-
+import { PlayersSearchComponent } from './players-search/players-search.component';
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'players', component: PlayersComponent },
-  { path: 'play', component: PlayComponent },
+	{ path: 'home', component: HomeComponent },
   { path: 'staff', component: StafflistComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  { path: 'players', component: PlayersComponent },
+  { path: 'profile', component: PlayersSearchComponent },
+	{ path: '', redirectTo: 'home', pathMatch: 'full'},
+	{ path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
