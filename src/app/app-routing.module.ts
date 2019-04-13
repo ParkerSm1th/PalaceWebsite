10b9632aec6eb4b842e1af9a8b539a4f8a743740
Isaac Goodwin(Flakenstine) <isaacgoodwin97@gmail.com>
2019-04-13 14:52:59 -0600

    Setup angular router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PlayersComponent } from './players/players.component';
-import { PlayComponent } from './play/play.component';
+import { StafflistComponent } from './stafflist/stafflist.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'players', component: PlayersComponent },
-  { path: 'play', component: PlayComponent},
+  { path: 'staff', component: StafflistComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
