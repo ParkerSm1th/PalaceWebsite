b2c27211b4fc1fe3684eea3de1110434e0d2d782
Vidsify <Vidsify@hotmail.com>
2019-04-19 21:38:55 +0100

    Parks Page Template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,14 @@ import { PlayersComponent } from './players/players.component';
 import { StafflistComponent } from './stafflist/stafflist.component';
 import { PlayComponent } from './play/play.component';
 import { PlayersSearchComponent } from './players-search/players-search.component';
+import { ParksComponent } from './parks/parks.component';
+
 const routes: Routes = [
 	{ path: 'home', component: HomeComponent },
   { path: 'staff', component: StafflistComponent },
   { path: 'players', component: PlayersComponent },
   { path: 'profile', component: PlayersSearchComponent },
+  { path: 'parks', component: ParksComponent},
 	{ path: '', redirectTo: 'home', pathMatch: 'full'},
 	{ path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
