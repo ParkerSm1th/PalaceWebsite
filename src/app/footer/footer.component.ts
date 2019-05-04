078a97b8cb4ed5cc37e16d32ffd6c81ea4d7ffc9
Isaac Goodwin(Flakenstine) <isaacgoodwin97@gmail.com>
2019-05-04 13:43:47 -0600

    More color adjustments, and fancy new social icons

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
-import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faYoutubeSquare } from '@fortawesome/free-brands-svg-icons';
-import { faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faTwitter, faYoutube, faInstagram, faDiscord} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'pl-footer',
@@ -13,17 +9,18 @@ import { faDiscord } from '@fortawesome/free-brands-svg-icons';
 export class FooterComponent implements OnInit {
   copyright: string;
   year: number;
-
-  faTwitterSquare = faTwitterSquare;
-  faFacebookSquare = faFacebookSquare;
+  faFacebookF = faFacebookF;
+  faTwitter = faTwitter;
+  faYoutube = faYoutube;
   faInstagram = faInstagram;
-  faYoutubeSquare = faYoutubeSquare;
   faDiscord = faDiscord;
+
+
 
   constructor() { }
 
   ngOnInit() {
-    this.copyright = "Copyright &copy; Palace Network";
+    this.copyright = 'Copyright &copy; Palace Network';
     this.year = Date.now();
   }
 
